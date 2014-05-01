c2b094b9c8c6fbea98beb498c9150e76cfadb270
Chris Roberts <chrisroberts.code@gmail.com>
2014-05-01 13:57:00 -0700

    Clean up chucker implementation

--- lib/fission-woodchuck/chucker.rb
+++ lib/fission-woodchuck/chucker.rb
@@ -2,28 +2,45 @@ module Fission
   module Woodchuck
     class Chucker < Fission::Callback
 
+      def setup(*_)
+        if(enabled?(:data))
+          require 'fission-data/init'
+        else
+          abort 'Data library is _required_ for woodchuck functionality'
+        end
+      end
+
       def valid?(*_)
         super do |m|
-          retreive(m, :data, :woodchuck) == true ||
-          [retreive(m, :data, :woodchuck, :filters)].flatten.compact.empty?
+          m.get(:data, :woodchuck)
         end
       end
 
       def execute(message)
         failure_wrap(message) do |payload|
-          if(retrieve(payload, :data, :woodchuck) == true)
-            payload[:data][:woodchuck] = {
-              :filters => Carnivore::Config.get(:fission, :woodchuck, :filters)
-            }
-            forward(payload)
-          else
+          unless(payload.get(:data, :woodchuck, :filters))
+            payload.set(:data, :woodchuck, :filters, Carnivore::Config.get(:fission, :woodchuck, :filters))
+          end
+          filters = payload.get(:data, :woodchuck, :filters)
+          if(filters.empty?)
             store!(payload)
-            job_completed('woodchuck', payload, message)
+            job_completed(:woodchuck, payload, message)
+          else
+            destination = filters.shift
+            transmit(destination, payload)
+            message.confirm!
           end
         end
       end
 
       def store!(payload)
+        Fission::Data::LogEntry.add(
+          :log => payload.get(:data, :woodchuck, :entry, :path),
+          :entry => payload.get(:data, :woodchuck, :entry, :content),
+          :entry_time => payload.get(:data, :woodchuck, :entry, :timestamp),
+          :tags => payload.get(:data, :woodchuck, :entry, :tags),
+          :source => payload.get(:data, :woodchuck, :entry, :source)
+        )
       end
 
     end
