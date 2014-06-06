2e447737ea996179764dbdcdb0ef12a07306c0aa
Chris Roberts <chrisroberts.code@gmail.com>
2014-06-06 12:08:01 -0700

    Add doc stuffs to version file

--- lib/fission-woodchuck/version.rb
+++ lib/fission-woodchuck/version.rb
@@ -1,7 +1,9 @@
 module Fission
   module Woodchuck
+    # Custom version class
     class Version < Gem::Version
     end
+    # Current library version
     VERSION = Version.new('0.1.0')
   end
 end
